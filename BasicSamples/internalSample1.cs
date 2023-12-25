f2be46e4606bcb18201d7862b093b8d4e9c573c8
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-25 10:43:45 +0200

    copied Foo hierarchy with new access modifiers

--- BasicSamples/internalSample1.cs
+++ BasicSamples/internalSample1.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Some_Inharitanse_Samples.BasicSamples
+namespace Some_Inharitanse_Samples.BasicSamples.Internal
 {
+    internal class Foo
+    {
+        internal string Name { get; set; }
+        internal int Age { get; set; }
+        internal int CheckAge() => Age;
+    }
+    internal class FooHeir : Foo
+    {
+        public string AccessCheck()
+        {
+            
+        }
+    }
     public class internalSample1
     {
+
     }
 }
