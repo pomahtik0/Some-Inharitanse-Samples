1ef70939af278cce255b6e0e347e39dd1fec841a
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-25 10:50:14 +0200

    access checks and overriding func

--- BasicSamples/internalSample1.cs
+++ BasicSamples/internalSample1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,18 @@ namespace Some_Inharitanse_Samples.BasicSamples.Internal
     {
         internal string Name { get; set; }
         internal int Age { get; set; }
-        internal int CheckAge() => Age;
+        internal virtual int CheckAge() => Age;
     }
     internal class FooHeir : Foo
     {
+        internal override int CheckAge()
+        {
+            return base.CheckAge();
+        }
+
         public string AccessCheck()
         {
-            
+            return "Можливий доступ, як до полів, так і до функцій";
         }
     }
     public class internalSample1
