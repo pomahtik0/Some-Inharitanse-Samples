accb2ac7916f6715d3977a16c297ddc5f0ee60f4
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-25 10:41:13 +0200

    new sample created

---
+++ BasicSamples/internalSample1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some_Inharitanse_Samples.BasicSamples
+{
+    public class internalSample1
+    {
+    }
+}
