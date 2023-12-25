350c0828ec16a2d9f8e90fce25714bb92ed21f40
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-25 10:25:41 +0200

    main Foo private fields set

--- private/privateSample1.cs
+++ private/privateSample1.cs
@@ -8,7 +8,9 @@ namespace Some_Inharitanse_Samples.Private
 {
     internal class Foo
     {
-
+        private string Name {  get; set; }
+        private int Age { get; set; }
+        private int CheckAge() => Age;
     }
     internal class FooHeir : Foo
     {
