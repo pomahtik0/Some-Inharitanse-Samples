30cb17662db86b76e759a421efc82d2476806c8f
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-25 10:12:06 +0200

    adding new sample file

---
+++ private/privateSample1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some_Inharitanse_Samples.Private
+{
+    internal class privateSample1
+    {
+
+    }
+}
