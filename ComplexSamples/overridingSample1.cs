6936f002fc54870caecd7c9b57e6eeaf780fd24f
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-27 11:03:25 +0200

    interface -> class -> class

---
+++ ComplexSamples/overridingSample1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some_Inharitanse_Samples.ComplexSamples
+{
+    interface I
+    {
+        internal void func();
+    }
+    class A : I
+    {
+        void I.func()
+        {
+            Console.WriteLine("A Class");
+        }
+    }
+    class B : A
+    {
+        void func()
+        {
+            Console.WriteLine("BClass");
+        }
+    }
+
+    internal class overridingSample1
+    {
+        public static void SampleMain()
+        {
+            I Aclass = new A();
+            I Bclass = new B();
+            Aclass.func();
+            Bclass.func();
+        }
+    }
+}
