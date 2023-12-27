d7411619c28b0a07537e5fbbcf62185647b2f403
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-27 09:11:31 +0200

    one more simple sample

--- BasicSamples/protectedSample1.cs
+++ BasicSamples/protectedSample1.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Some_Inharitanse_Samples.BasicSamples.Protected
 {
+    internal class Foo
+    {
+        protected string Name { get; set; }
+        protected int Age { get; set; }
+        protected virtual int CheckAge() => Age;
+    }
+    internal class FooHeir : Foo
+    {
+        protected override int CheckAge()
+        {
+            return base.CheckAge();
+        }
+
+        public string AccessCheck()
+        {
+            return "Можливий доступ, як до полів, так і до функцій";
+        }
+    }
+
     internal class protectedSample1
     {
+        public static void AccessCheck()
+        {
+            FooHeir heir = new FooHeir();
+            Console.WriteLine("Неможливий доступ ні до полів ні до функцій.");
+        }
     }
 }
