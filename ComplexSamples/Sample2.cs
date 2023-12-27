47915940116a88d20a261f1c67dc6186af951615
Roman Havriluk <pomahtik_inc@outlook.com>
2023-12-27 11:24:33 +0200

    Inerface -> abstract class (no-realization) -> class

---
+++ ComplexSamples/Sample2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Some_Inharitanse_Samples.ComplexSamples.Sample2
+{
+    internal interface I
+    {
+        public void func();
+    }
+    internal abstract class A:I
+    { 
+        public abstract void func();
+    }
+    internal class B : A
+    {
+        public override void func()
+        {
+            Console.WriteLine("BClass");
+        }
+    }
+    internal class C : I
+    {
+        public void func()
+        {
+            Console.WriteLine("CClass");
+        }
+    }
+    internal class Sample2
+    {
+        public static void SampleMain()
+        {
+            I bclass = new B();
+            I cclass = new C();
+            bclass.func();
+            cclass.func();
+        }
+    }
+}
